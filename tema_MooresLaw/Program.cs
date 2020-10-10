ad1f29a7741836db5fa1359dfc71519dd2ab8151
Floruta Raul <raulbomberi@yahoo.com>
2020-10-10 23:31:47 +0300

    Moores Law

---
+++ tema_MooresLaw/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tema_MooresLaw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Console.Read();
+            int Luni = 0;
+            int putere = 1;
+
+            do
+            {
+                Luni += 18;
+                putere *= 2;
+            } while (putere<=n);
+
+            Console.WriteLine(Luni);
+        }
+    }
+}
