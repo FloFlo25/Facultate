aa72e96c9effc74a77ab522b7acf853e08c56038
Floruta Raul <floruta.raul25@gmail.com>
2021-06-20 20:40:14 +0300

    Added Project Numere Rationale and deleted empty folder (Numere_rationale)

---
+++ Anul 1/Semestrul 2/Programare orientata pe obiecte/Numere Rationale/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numere_Rationale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var rational1 = new Rational(2, 3);
+            var rational2 = new Rational(2, 3);
+            var rational3 = new Rational(2, 4);
+
+            Console.WriteLine(rational1.adunare(rational2));
+            Console.WriteLine(rational1.adunare(rational3));
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Anul 1/Semestrul 2/Programare orientata pe obiecte/Numere Rationale/Rational.cs
@@ -0,0 +1,96 @@
+﻿namespace Numere_Rationale
+{
+    class Rational
+    {
+        #region Properties
+        private int numarator;
+
+        public int Numarator
+        {
+            get { return numarator; }
+            set { numarator = value; }
+        }
+
+        private int numitor;
+
+        public int Numitor
+        {
+            get { return numitor; }
+            set { numitor = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public Rational()
+        {
+
+        }
+
+        public Rational(int numarator, int numitor)
+        {
+            Numarator = numarator;
+            Numitor = numitor;
+        }
+        #endregion
+
+        #region Methods
+        public Rational adunare(Rational fractie2)
+        {
+            int numitorComun = cmmdc(this.Numitor, fractie2.Numitor);
+
+            if (this.Numitor != fractie2.Numitor)
+            {
+                return new Rational(this.Numarator*fractie2.Numitor + this.Numitor*fractie2.Numarator, numitorComun);
+            }
+            else
+            {
+                return new Rational(this.Numarator + fractie2.Numarator, fractie2.Numitor);
+            }
+        }
+
+        public override string ToString()
+        {
+            if (isDivizibil(Numarator, Numitor))
+            {
+                return $"{Numarator / Numitor}";
+            }
+            else
+            {
+                return $"{Numarator}/{Numitor}";
+            }
+        }
+
+        public bool isDivizibil(int numarator, int numitor)
+        {
+            if (numarator % numitor == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int cmmdc(int a,int b)
+        {
+            int x = a;
+            int y = b;
+
+            while (x!=y)
+            {
+                if (x>y)
+                {
+                    x = x - y;
+                }
+                else
+                {
+                    y = y - x;
+                }
+            }
+
+            return (a * b) / x;
+        }
+        #endregion
+    }
+}
