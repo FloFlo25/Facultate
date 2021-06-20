b2e305daed5a1fdfd2eff4646022940d159e6dce
Floruta Raul <floruta.raul25@gmail.com>
2021-06-20 20:05:59 +0300

    Added project <Numere complexe>

---
+++ Anul 1/Semestrul 2/Programare orientata pe obiecte/Numere Complexe/Complex.cs
@@ -0,0 +1,84 @@
+﻿namespace Numere_Complexe
+{
+    class Complex
+    {
+        #region Properties
+        private int parteaReala;
+
+        public int ParteaReala
+        {
+            get { return parteaReala; }
+            set { parteaReala = value; }
+        }
+
+        private int parteaImaginara;
+
+        public int ParteaImaginara
+        {
+            get { return parteaImaginara; }
+            set { parteaImaginara = value; }
+        }
+        #endregion
+
+
+        #region Constructors
+        public Complex()
+        {
+
+        }
+
+        public Complex(int parteaReala, int parteaImaginara) : this()
+        {
+            ParteaReala = parteaReala;
+            ParteaImaginara = parteaImaginara;
+        }
+        #endregion
+
+
+        public Complex adunare(Complex complex)
+        {
+            this.ParteaReala += complex.ParteaReala;
+            this.ParteaImaginara += complex.ParteaImaginara;
+
+            return this;
+        }
+
+        public Complex scadere(Complex complex)
+        {
+            this.ParteaReala -= complex.ParteaReala;
+            this.ParteaImaginara -= complex.ParteaImaginara;
+
+            return this;
+        }
+
+        public Complex impartire(Complex complex)
+        {
+            this.ParteaReala /= complex.ParteaReala;
+            this.ParteaImaginara /= complex.ParteaImaginara;
+
+            return this;
+        }
+
+        public Complex inmultire(Complex complex)
+        {
+            this.ParteaReala *= complex.ParteaReala;
+            this.ParteaImaginara *= complex.ParteaImaginara;
+
+            return this;
+        }
+
+        public override string ToString()
+        {
+            if (ParteaImaginara > 0)
+            {
+                return $"{ParteaReala} + {ParteaImaginara}i";
+            }
+            else
+            {
+                return $"{ParteaReala} - {ParteaImaginara}i";
+            }
+
+        }
+
+    }
+}
---
+++ Anul 1/Semestrul 2/Programare orientata pe obiecte/Numere Complexe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Numere_Complexe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var complex1 = new Complex(2, 3);
+            var complex2 = new Complex(5, 4);
+
+
+            Console.WriteLine("Adunare: " + complex1.adunare(complex2));
+            Console.WriteLine("Scadere: " + complex1.scadere(complex2));
+            Console.WriteLine("Inmultire: " + complex1.inmultire(complex2));
+            Console.WriteLine("Impartire: " + complex1.impartire(complex2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
