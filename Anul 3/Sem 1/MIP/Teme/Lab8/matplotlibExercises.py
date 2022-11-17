b4fa4dc71372ebdd8396191707de65c5c743f7a7
Raul <floruta.raul25@gmail.com>
2022-11-17 10:57:39 +0200

    numpy and matlab

---
+++ Anul 3/Sem 1/MIP/Teme/Lab8/matplotlibExercises.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xPoints = np.array([0,10])
+yPoints = np.array([0,10])
+
+plt.plot(xPoints,yPoints)
+plt.show()
---
+++ Anul 3/Sem 1/MIP/Teme/Lab8/numpyExercises.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def print_matrix_without_brackets(two_dimensional_array):
+    print(str(two_dimensional_array).replace(' [', '').replace('[', '').replace(']', ''))
+
+matrixOne = np.array([[1, 2], [3, 4]])
+matrixTwo = np.array([[1, 1], [2, 0]])
+product = np.dot(matrixOne, matrixTwo)
+
+print("Matrix 1")
+print_matrix_without_brackets(matrixOne)
+print("Matrix 2")
+print_matrix_without_brackets(matrixTwo)
+
+print()
+
+
+print("Product of the two matrixes")
+print_matrix_without_brackets(product)
